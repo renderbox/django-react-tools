ded860abecddfc57f5109b2a96854b6164d90de4
Grant Viklund <renderbox@gmail.com>
2019-02-20 10:08:45 -0800

    Initial Project Add

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Django React Tools documentation master file, created by
+   sphinx-quickstart on Wed Feb 20 10:01:02 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Django React Tools's documentation!
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,50 @@
+# --------------------------------------------
+# Copyright 2019, Grant Viklund
+# @Author: Grant Viklund
+# @Date:   2019-1-22 13:50:49
+# --------------------------------------------
+
+from os import path
+from setuptools import setup
+
+file_path = path.abspath(path.dirname(__file__))
+with open(path.join(file_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+package_metadata = {
+    'name': 'django-react-tools',
+    'version': '0.1.0',
+    'description': 'Tools for helping integrate ReactJS into a Django project.',
+    'long_description': long_description,
+    'url': 'https://github.com/renderbox/django-react-tools',
+    'author': 'Grant Viklund',
+    'author_email': 'renderbox@gmail.com',
+    'license': '',
+    'classifiers': [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+}
+
+setup(
+    **package_metadata,
+    packages=['reacttools'],
+    python_requires=">=3.6",
+    install_requires=[
+        'Django>=2.1.5',
+        ],
+    extras_require={
+        'dev': [],
+        'test': [],
+        'prod': [],
+        'build': [],
+        'docs': [
+            'coverage==4.4.1',
+            'Sphinx==1.6.4'],
+    }
+)
