eab3f35c4242624348597ad78a64833be68aa7c0
David Doughty <davidd@whitemoondreams.com>
2020-07-28 15:31:28 -0700

    Update version to 0.2.10

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = 'Django React Tools'
 author = 'Grant Viklund'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.9'
+release = '0.2.10'
 
 # -- General configuration ------------------------------------------------
 
