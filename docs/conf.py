25d6f6b94289ae65393ce7573270b04cc0e956bc
David Doughty <davidd@whitemoondreams.com>
2021-03-16 17:40:31 -0700

    Bump version to 0.2.13

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = 'Django React Tools'
 author = 'Grant Viklund'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.12'
+release = '0.2.13'
 
 # -- General configuration ------------------------------------------------
 
