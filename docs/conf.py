117a677963e4763728742e1b41a62bf98b744c9e
DeVonJ <devonj@whitemoondreams.com>
2021-09-30 15:39:01 +0100

    updats version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = 'Django React Tools'
 author = 'Grant Viklund'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.13'
+release = '0.2.14'
 
 # -- General configuration ------------------------------------------------
 
