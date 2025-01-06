7316d0cee873ab244a6acab3dbf8e4fda7b92a3e
Grant Viklund <grant.viklund@gmail.com>
2025-01-06 12:17:08 -0500

    More cleanup and reformatting with black

--- develop/develop/urls.py
+++ develop/develop/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from reacttools.views import IndexView, ReactAppView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dev/', IndexView.as_view()),
-    path('reacttools/', include('reacttools.urls')),
-    path('', ReactAppView.as_view()),
+    path("admin/", admin.site.urls),
+    path("dev/", IndexView.as_view()),
+    path("reacttools/", include("reacttools.urls")),
+    path("", ReactAppView.as_view()),
 ]
